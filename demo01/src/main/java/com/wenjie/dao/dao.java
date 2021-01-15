3654ddb52dedc88222ee25a2cbe07a790a08e43a
HWJ <1239246306@qq.com>
2021-01-15 21:37:59 +0800

    完成了一些新的功能

--- demo01/src/main/java/com/wenjie/dao/dao.java
+++ demo01/src/main/java/com/wenjie/dao/dao.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface dao {
 
     public List<user> getall();
+
+
+
 }
---
+++ demo01/src/main/java/com/wenjie/dao/take.java
@@ -0,0 +1,8 @@
+package src.main.java.com.wenjie.dao;
+
+public class take {
+    public static void main(String[] args) {
+        System.out.println("这是一个练习");
+        System.out.println("完成了一个新的功能");
+    }
+}
